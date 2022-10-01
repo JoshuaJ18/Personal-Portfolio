b30298e75d805f5005207306b52c0534bcde0610
JoshuaJ18 <109106928+JoshuaJ18@users.noreply.github.com>
2022-10-01 00:17:35 -0400

    Complete Navigation Bar
    Completed the navigation bar and added mobile support/responsiveness. The only issue is the bar items may not be centered on all screens, but the issue can be revisited later.

--- website/src/components/navigation/Navbar.js
+++ website/src/components/navigation/Navbar.js
@@ -1,12 +1,22 @@
 import { Component } from "react";
 import { MenuData } from "./MenuData";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 import "./NavbarStyles.css";
 
 class Navbar extends Component {
+  state = { clicked: false };
+  handleClick =()=>{
+    this.setState({clicked: !this.state.clicked})
+  }
   render() {
     return(
-      <nav className="NavbarItems">
-        <ul className="nav-menu">
+      <nav className="navbar-items">
+        <div className="menu-icons" onClick={this.handleClick}>
+        <FontAwesomeIcon icon= { this.state.clicked ? faXmark : faBars
+        }  />
+        </div>
+        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
           {MenuData.map((item, index)=>{
             return(
               <li key={index}>
