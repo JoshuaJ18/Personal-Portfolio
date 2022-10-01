994614d3a280404c9b2d13d20e10d88d41f7cfaa
JoshuaJ18 <109106928+JoshuaJ18@users.noreply.github.com>
2022-09-30 21:45:09 -0400

    Basic Navigation Bar
    Nav bar with no mobile responsiveness

---
+++ website/src/components/navigation/Navbar.js
@@ -0,0 +1,26 @@
+import { Component } from "react";
+import { MenuData } from "./MenuData";
+import "./NavbarStyles.css";
+
+class Navibar extends Component {
+  render() {
+    return(
+      <nav className="NavbarItems">
+        <ul className="nav-menu">
+          {MenuData.map((item, index)=>{
+            return(
+              <li key={index}>
+                <a href={item.url} className={item.cName}>
+                  {item.title}
+                </a>
+              </li>
+            )
+          })}
+    
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navibar;
