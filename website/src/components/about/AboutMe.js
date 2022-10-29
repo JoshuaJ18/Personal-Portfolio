631ff8aa016a08584f216d853f8451c80474d923
JoshuaJ18 <109106928+JoshuaJ18@users.noreply.github.com>
2022-10-28 22:18:58 -0400

    Full Scrolling Feature Overhaul
    First feature update is that now when a navbar item is clicked, it scrolls to the part of the page instead of jumping to it.
    
    The next feature is the page animations on scroll that use the AOS library to make the page better.

--- website/src/components/about/AboutMe.js
+++ website/src/components/about/AboutMe.js
@@ -1,27 +1,31 @@
-import { Component } from "react";
+import { useEffect } from "react";
+import aos from "aos";
+import "aos/dist/aos.css";
 import './AboutMeStyles.css';
 
-class AboutMe extends Component {
-  render() {
-    return(
-      <div className="about-container">
-        <div className="header">
-          <h1 id="AboutMe">About Me</h1>
-        </div>
-        <div className="paragraph">
-          <p>I am a Sophomore Computer Science Major who loves to learn!
-          I have experience with many languages, such as Java, Python, C++, 
-          and Javascript. I also have experience with some Javascript frameworks 
-          like Vue and React. I am currently taking Object Oriented Programming in 
-          Java and will be taking Data Structures and Algorithms in Spring 2023. 
-          <br></br><br></br>Currently my interests are Front End Development, Full Stack Development,
-          and Embedded Programming. I am learning a lot of new things within these specialties
-          and hoping to gain experience through internships to narrow it down to one specialty
-          before graduation.</p>
-       </div>
+function AboutMe() {
+  useEffect(() => {
+    aos.init({duration: 800});
+  }, []);
+  return(
+    <div className="about-container">
+      <p className="hidden" id="AboutMe">A</p>
+      <div className="header">
+         <h1 data-aos="zoom-in">About Me</h1>
       </div>
-    );
-  }
+      <div className="paragraph" data-aos="zoom-in-up">
+        <p>I am a Sophomore Computer Science Major who loves to learn!
+        I have experience with many languages, such as Java, Python, C++, 
+        and Javascript. I also have experience with some Javascript frameworks 
+        like Vue and React. I am currently taking Object Oriented Programming in 
+        Java and will be taking Data Structures and Algorithms in Spring 2023. 
+        <br></br><br></br>Currently my interests are Front End Development, Full Stack Development,
+        and Embedded Programming. I am learning a lot of new things within these specialties
+        and hoping to gain experience through internships to narrow it down to one specialty
+        before graduation.</p>
+      </div>
+    </div>
+  );
 }
 
 export default AboutMe;
